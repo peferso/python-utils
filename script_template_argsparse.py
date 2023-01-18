7297d7a1bbfe025619d815711cadc1b5665fe240
peferso <p.fdez.s.90@gmail.com>
2023-01-18 17:17:39 +0100

    20230118 - 0 - template script with argsparse

---
+++ script_template_argsparse.py
@@ -0,0 +1,72 @@
+# %%
+import argparse
+import sys
+import logging
+
+
+def create_logger():
+    logger = logging.getLogger(__name__)
+    handlers = logger.handlers
+    if len(handlers) < 1:
+        fh = logging.StreamHandler(sys.stdout)
+        fh_formatter = logging.Formatter(
+            "[%(levelname)s] [%(asctime)s] [%(funcName)s] %(message)s")
+        fh.setFormatter(fh_formatter)
+        logger.addHandler(fh)
+        logger.setLevel(logging.DEBUG)
+    return logger
+
+logger = create_logger()
+
+
+class Actions():
+    """
+    Class wrapping up the possible script tasks.
+
+    Parameters
+    ----------
+    Args:
+        input_args (argparse.ArgumentParser): argument parser object.
+    """
+    def __init__(self, input_args):
+        self._input_parameter = input_args.input_parameter
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    """Initialize the Argument Parser to handle the input arguments passed
+    to the script.
+
+    Returns:
+        argparse.ArgumentParser: argument parser object
+    """
+    parser = argparse.ArgumentParser(
+       usage='%(prog)s run -o [OPTION]',
+       description='Some useful description of the script usage.'
+    )
+    parser.add_argument(
+        'run',
+        nargs='?',
+        help='The action or task to run.',
+        type=str,
+        choices=[
+            'action_1',
+            'action_2',
+        ]
+    )
+    parser.add_argument(
+        '-report_file_path',
+        nargs='?',
+        help='Path to the json file.',
+        type=str,
+        default=None
+    )
+    return parser
+
+if __name__ == '__main__':
+    parser = init_argparse()
+    args = parser.parse_args()
+    if not (args.run):
+        parser.print_help()
+        sys.exit(1)
+    else:
+        run(args)
