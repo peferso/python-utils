91422a0ad65a3918d49c077a3b86121ec4773c13
peferso <p.fdez.s.90@gmail.com>
2023-02-28 14:50:50 +0100

    2023-02-28 14:50:50 -

--- timezones_pandas.py
+++ timezones_pandas.py
@@ -39,3 +39,19 @@ df['date_ElSalvador'] = df['date_UTC'].dt.tz_convert('America/El_Salvador')
 df['date_CST_2'] = pd.to_datetime(
     df['date_str'], utc=True
 ).dt.tz_convert('America/El_Salvador')
+
+
+# %%
+# Get the difference in hours betwee two timezones given a input date time.
+#
+def get_hours_dif(
+    timezone_A: str,
+    timezone_B: str,
+    date_time: pd.api.types.DatetimeTZDtype
+) -> float:
+    datetime_A = pd.Series(date_time).dt.tz_localize(timezone_A)
+    datetime_B = datetime_A.dt.tz_convert(timezone_B)
+    _datetime_A = pd.to_datetime(datetime_A.dt.strftime('%Y-%m-%d %H:%M:%S'))
+    _datetime_B = pd.to_datetime(datetime_B.dt.strftime('%Y-%m-%d %H:%M:%S'))   
+    time_dif = (_datetime_B - _datetime_A).astype('timedelta64[h]')
+    return time_dif.values[0]
